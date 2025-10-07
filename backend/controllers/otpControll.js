2368168cf2914584bbf00979dc5d69374b27f985
sai sarayu <sarayu2926@gmail.com>
2025-10-07 16:45:55 +0530

    done

--- backend/controllers/otpControll.js
+++
@@ -1,9 +0,0 @@
-const nodemailer = require("nodemailer");
-const transporter = nodemailer.createTransport({
-    service: "gmail",
-    auth: {
-      user: "saisarayu21@gmail.com",
-      pass: "vuaz pakz vaim gomi"
-    }
-  });
-  
