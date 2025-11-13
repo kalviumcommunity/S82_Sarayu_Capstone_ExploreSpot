140ca44172b27a5dc7d6155fbaa81c32fbe5ac55
sai sarayu <sarayu2926@gmail.com>
2025-11-13 17:26:05 +0530

    done

---
+++ backend/model/Spot.js
@@ -0,0 +1,13 @@
+const mongoose = require("mongoose");
+
+const SpotSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    location: { type: String, required: true },
+    description: { type: String, required: true },
+    imageUrl: { type: String },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Spot", SpotSchema);
